6dfc7a8d233aac2c45900d945066d48079394df8
recent <mcjos75@yahoo.co.uk>
2016-02-27 11:19:09 +0000

    Commit 270216

---
+++ MeetingImpl.java
@@ -0,0 +1,54 @@
+package ContactManager;
+
+import java.util.Calendar;
+import java.util.Set;
+
+public abstract class MeetingImpl implements Meeting{
+	
+	public int id;
+	public Calendar date;
+	public Set<Contact> contact;
+	
+	
+	public MeetingImpl (int id, Calendar date, Set<Contact> contact ) {
+		if(contact == null) {
+			throw new NullPointerException ();
+		}
+		
+		else if (contact.size() < 1){
+			throw new IllegalArgumentException ();
+		}
+		
+		else if (id <= 0) {
+			throw new IllegalArgumentException ();
+		}
+		
+		else if (date == null) {
+			throw new NullPointerException ();
+		}
+			
+		this.id = id;
+		this.date = date;
+		this.contact = contact;
+
+	}
+	
+
+	@Override
+	public int getId() {
+		return id;
+	}
+
+	
+	@Override
+	public Calendar getDate() {
+		return date;
+	}
+
+	
+	@Override
+	public Set<Contact> getContacts() {
+		return contact;
+	}
+
+}
